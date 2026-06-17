1a91ac92ccd5d48296b09e6841966f1ea195e7e5
Tianqi Wang <tianqi.wang@vitesco.com>
2026-06-17 22:03:15 +0800

    Fix set port data type for enum

--- work/scripts/data_mng/src/autoSetPortDataTypes.m
+++ work/scripts/data_mng/src/autoSetPortDataTypes.m
@@ -320,6 +320,7 @@ function map = loadPrefixMapping(mappingFile)
         map('bool') = 'boolean';
         map('b')   = 'boolean';
         map('enum') = 'Enum: auto';
+        map('e')   = 'Enum: auto';
     end
 end
 
