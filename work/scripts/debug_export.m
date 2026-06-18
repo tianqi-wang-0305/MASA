e2e4e5d637e0aa2ee1c38581af66acd39d766260
tianqi-wang-0305 <tianqi.wang@vitesco.com>
2026-06-18 22:36:16 +0800

    Remove debug scripts

--- work/scripts/debug_export.m
+++
@@ -1,4 +0,0 @@
-addpath('/Users/wangtianqi/Desktop/autoModeling/work/scripts/data_mng/src');
-r = exportSignalsToExcel('/Users/wangtianqi/Desktop/autoModeling/work/tst_mdl/Foc_2024b.slx');
-fprintf('Result: %d in, %d out\n', r.inputCount, r.outputCount);
-exit(0);
